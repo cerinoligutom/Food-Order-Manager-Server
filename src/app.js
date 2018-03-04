0c496fb370df3d3784c35269ba1929992f5f5f04
Cerino O. Ligutom III <zeferinix@gmail.com>
2018-03-04 15:00:09 +0800

    Setup basic environment

---
+++ src/app.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-console */
+let express = require('express');
+let graphqlHTTP = require('express-graphql');
+let compression = require('compression');
+import { buildSchema } from 'graphql';
+import chalk from 'chalk';
+
+app.use(compression());
+
+let schema = buildSchema(`
+    type Query {
+        hello: String
+    }
+`);
+
+let root = { hello: () => 'Hello World!' };
+
+let app = express();
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}));
+
+app.listen(3000, () => {
+    console.log(chalk.green('Server is up on localhost:3000/graphql'));
+});
